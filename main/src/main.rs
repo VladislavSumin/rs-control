3d396e1c2494df8ed5892a71101d344134bff99e
Vladislav Sumin <vladislavsumin@mail.ru>
2022-10-10 21:49:12 +0300

    create minimal hyper server configuration

--- main/src/main.rs
+++ main/src/main.rs
@@ -2,10 +2,12 @@ use std::error::Error;
 use tracing::info;
 
 mod logger;
+mod web_server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let _guard = logger::init();
     info!("Starting server");
+    web_server::run().await?;
     Ok(())
 }
---
+++ main/src/web_server.rs
@@ -0,0 +1,26 @@
+use std::convert::Infallible;
+use std::error::Error;
+use std::net::SocketAddr;
+use hyper::{Body, Response};
+use hyper::service::{make_service_fn, service_fn};
+use tracing::info;
+
+pub async fn run() -> Result<(), Box<dyn Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    info!("Server listen at {}", addr);
+
+    let make_svc = make_service_fn(|_| {
+        async {
+            Ok::<_, Infallible>(
+                service_fn(|_| {
+                    async {
+                        Ok::<Response<Body>, Infallible>(Response::new(Body::from("Test")))
+                    }
+                })
+            )
+        }
+    });
+
+    hyper::Server::bind(&addr).serve(make_svc).await?;
+    Ok(())
+}
