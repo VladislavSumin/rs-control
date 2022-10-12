c20d21cc47238e46f9cbc2af1824c43ef2a4471d
Vladislav Sumin <vladislavsumin@mail.ru>
2022-10-12 19:45:33 +0300

    [ca] add save/load

--- main/src/ca.rs
+++ main/src/ca.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::path::Path;
+use std::fs;
 use openssl::asn1::Asn1Time;
 use openssl::error::ErrorStack;
 use openssl::hash::MessageDigest;
@@ -5,6 +8,10 @@ use openssl::pkey::{PKey, Private};
 use openssl::rsa::Rsa;
 use openssl::x509::X509;
 
+const CERT_SUB_PATH: &str = "cert.pem";
+const KEY_SUB_PATH: &str = "key.der";
+
+#[derive(Debug)]
 pub struct CA {
     cert: X509,
     key: PKey<Private>,
@@ -27,15 +34,47 @@ impl CA {
         let ca = CA { cert, key: private_key };
         Ok(ca)
     }
+
+    pub fn load(dir: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
+        let dir = dir.as_ref();
+        let cert = X509::from_pem(&fs::read(dir.join(CERT_SUB_PATH))?)?;
+        let key = PKey::private_key_from_der(&fs::read(dir.join(KEY_SUB_PATH))?)?;
+
+        let ca = CA { cert, key };
+
+        Ok(ca)
+    }
+
+    pub fn save(&self, dir: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
+        let dir = dir.as_ref();
+
+        fs::write(dir.join(CERT_SUB_PATH), self.cert.to_pem()?)?;
+        fs::write(dir.join(KEY_SUB_PATH), self.key.private_key_to_der()?)?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::CA;
+    use std::fs;
 
     #[test]
     fn create_ca() {
         let ca = CA::new(1).unwrap();
-        println!("{:#?}", ca.cert);
+        println!("{:#?}", ca);
+    }
+
+    #[test]
+    fn save_load_ca() {
+        const TEST_FOLDER: &str = "./test/ca/save_load_ca/";
+        let _ = fs::remove_dir_all(TEST_FOLDER);
+        fs::create_dir_all(TEST_FOLDER).unwrap();
+        let original_ca = CA::new(1).unwrap();
+        original_ca.save(TEST_FOLDER).unwrap();
+        let loaded_ca = CA::load(TEST_FOLDER).unwrap();
+        assert_eq!(original_ca.cert, loaded_ca.cert);
+        fs::remove_dir_all(TEST_FOLDER).unwrap();
     }
 }
