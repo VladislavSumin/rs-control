880f4e5f2393bad1b7b609676365c81f8312db5e
Vladislav Sumin <vladislavsumin@mail.ru>
2022-10-14 22:45:30 +0300

    add ca loading

--- main_sever/src/main.rs
+++ main_sever/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use tracing::info;
+use crate::ca::{CA, CAError};
 
 mod logger;
 mod web_server;
@@ -9,6 +10,33 @@ mod ca;
 async fn main() -> Result<(), Box<dyn Error>> {
     let _guard = logger::init();
     info!("Starting server");
+
+    let _ = load_ca().await?;
+
     web_server::run().await?;
     Ok(())
+}
+
+async fn load_ca() -> Result<CA, CAError> {
+    const CA_DIR: &str = "./ssl/";
+    const CA_DAYS_VALID: u32 = 10 * 365;
+    info!("Loading CA");
+    let ca = CA::load(CA_DIR).or_else(|e| {
+        match &e {
+            // TODO make better match
+            CAError::OpenSslError(_) => Err(e),
+            CAError::IOError(io_err) => match io_err.kind() {
+                std::io::ErrorKind::NotFound => {
+                    info!("CA not found, creating new");
+                    let _ = std::fs::create_dir_all(CA_DIR);
+                    let ca = CA::new(CA_DAYS_VALID)?;
+                    ca.save("./ssl")?;
+                    Ok(ca)
+                }
+                _ => Err(e),
+            },
+        }
+    })?;
+    info!("CA loaded");
+    Ok(ca)
 }
